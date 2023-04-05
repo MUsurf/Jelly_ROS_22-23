79b83df19902ebbf86abb2de30782827945f4a1a
MUSURF <muengrsurf@umsystem.edu>
2023-04-05 17:35:01 -0500

    working on pioled install script

--- OLED/install_oled_ip_script.sh
+++ OLED/install_oled_ip_script.sh
@@ -2,6 +2,8 @@
 
 echo -e "\nInstalling oled_ip.py to start at boot\n"
 
+sudo pip3 install adafruit-circuitpython-ssd1306
+sudo apt-get install python3-pil
 sudo cp /home/surf/Jelly_ROS_22-23/OLED/pioled_stats.service /etc/systemd/system/pioled_ip.service
 sudo systemctl enable --now pioled_ip
 
